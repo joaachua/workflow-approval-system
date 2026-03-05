fb1727a3dadd714401b105d9051a261ad9034df0
joaachua <ajoannachua@gmail.com>
2026-03-05 22:01:42 +0800

    - minor changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const config = require("./config");
 
 const app = express();
 
-app.use(cors({ origin: config.frontendUrl }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", require("./routes"));
